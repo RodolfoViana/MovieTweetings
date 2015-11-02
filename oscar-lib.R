eae995575853d44ad69c788ce5978b31eea518b2
rodolfoviana <rodolfodelimaviana@gmail.com>
2015-11-01 21:20:17 -0300

    Oscar nominations

---
+++ oscar-lib.R
@@ -0,0 +1,24 @@
+# Return the mean of ratings before and after oscar nominations 
+
+mean_before_after <- function(movies_ratings, year_movie, date_nominations, nominations) {
+  before_oscar <- movies_ratings  %>% 
+    filter(rating_timestamp > date_nominations)  %>% 
+    filter(movie_id %in% filter(nominations, year == year_movie)$cod_imdb)
+  
+  after_oscar <- movies_ratings  %>% 
+    filter(rating_timestamp < date_nominations) %>% 
+    filter(movie_id %in% filter(nominations, year == year_movie)$cod_imdb)
+  
+  group_before_oscar <- group_by(before_oscar, movie_id)
+  group_after_oscar <- group_by(after_oscar, movie_id)
+  
+  before_oscar_mean <- summarise(group_before_oscar, mean(rating))
+  colnames(before_oscar_mean) <- c("movie_id", "ratings_before")
+  
+  after_oscar_mean <- summarise(group_after_oscar, mean(rating))
+  colnames(after_oscar_mean) <- c("movie_id", "ratings_after")
+  
+  oscar <- cbind(before_oscar_mean, after_oscar_mean[,2])
+  
+  left_join(oscar, nominations, by = c("movie_id" = "cod_imdb"))
+}
