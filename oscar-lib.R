17bd46e43ec5537d4c680c237c286c5e13c06807
rodolfoviana <rodolfodelimaviana@gmail.com>
2015-11-08 21:34:18 -0300

    Best Movie

--- oscar-lib.R
+++ oscar-lib.R
@@ -2,11 +2,11 @@
 
 mean_before_after <- function(movies_ratings, year_movie, date_nominations, nominations) {
   before_oscar <- movies_ratings  %>% 
-    filter(rating_timestamp > date_nominations)  %>% 
+    filter(rating_timestamp < date_nominations)  %>% 
     filter(movie_id %in% filter(nominations, year == year_movie)$cod_imdb)
   
   after_oscar <- movies_ratings  %>% 
-    filter(rating_timestamp < date_nominations) %>% 
+    filter(rating_timestamp > date_nominations) %>% 
     filter(movie_id %in% filter(nominations, year == year_movie)$cod_imdb)
   
   group_before_oscar <- group_by(before_oscar, movie_id)
@@ -21,4 +21,14 @@ mean_before_after <- function(movies_ratings, year_movie, date_nominations, nomi
   oscar <- cbind(before_oscar_mean, after_oscar_mean[,2])
   
   left_join(oscar, nominations, by = c("movie_id" = "cod_imdb"))
+}
+
+mean_before <- function(movies_ratings, date, nominations) {
+  before_date <- movies_ratings  %>% 
+    filter(rating_timestamp > date)
+  
+  group_before <- group_by(before_date, movie_id)
+  
+  before_mean <- summarise(group_before, mean(rating))
+  colnames(before_oscar_mean) <- c("movie_id", "ratings_before")
 }
